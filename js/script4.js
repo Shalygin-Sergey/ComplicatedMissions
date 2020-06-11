3ab86942d7461376e9712f72023dae4d52455dae
Shalygin-Sergey <60192169+Shalygin-Sergey@users.noreply.github.com>
2020-06-11 11:22:43 +0300

    Вывел в столбик все простые числа от 1 до 100

--- js/script4.js
+++ js/script4.js
@@ -7,3 +7,16 @@ arr.forEach((item) => {
         console.log(item);
     }
 });
+
+function isPrime(n) {
+    for (let i = 2; i * i <= n; i === 2 ? i++ : i += 2) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+    return n > 1;
+}
+
+const res = [...Array(100)].reduce((a, _, i) =>
+    a.concat(isPrime(i) ? `Делители числа ${i}: 1 и ${i}` : []), []).join('\n');
+console.log(res);
